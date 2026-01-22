55f579ca3e752d7773955c526aed099b8ea46d5b
HectorOdato <leoodato@gmail.com>
2026-01-21 23:19:23 -0300

    implemente nuevas iimagenes para diseño de albumpage

---
+++ src/assets/images/imgbanners.jsx
@@ -0,0 +1,14 @@
+//Eras banners images
+
+export const imgdebutbanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047772/1_al8osl.png";
+export const imgfearlessbanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047772/2_kxqxdy.png";
+export const imgspeaknowbanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047772/3_jrnttr.png";
+export const imgredbanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047772/4_mjmqqs.png";
+export const img1989banner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047772/5_shwxhu.png";
+export const imgreputationbanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047772/6_grmwe5.png";
+export const imgloverbanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047772/7_vu2k2j.png";
+export const imgfolklorebanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047773/8_qlm9vt.png";
+export const imgevermorebanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047773/9_plftdm.png";
+export const imgmidnightsbanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047772/10_gltqrj.png";
+export const imgtorturedpoetsbanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1768734863/tortured_poets_banner_fqv3u3.png";
+export const imglifeofashowgirlbanner = "https://res.cloudinary.com/deijzj2mb/image/upload/v1769047773/tloasbanner_sbzl58.jpg";
